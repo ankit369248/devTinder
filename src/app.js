38eac2ad442b4a629411bdcb7c249de50e2a91e9
ankit99 <chouhanankit2020@gmail.com>
2025-10-11 21:50:19 +0530

    Added Express routes practice (GET, POST, PUT, DELETE, params & queries)

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const app = express();
 const port = 7777;
 
 //Request handler
-app.get("/", (req, res) => {
+app.get("/user/:userId", (req, res) => {
+  console.log(req.query);
+  console.log(req.params);
   res.send("Hello ji!!");
 });
 
@@ -17,6 +19,22 @@ app.get("/getWishes", (req, res) => {
   res.send("Hello Hope you are doing well!!");
 });
 
+app.post("/userdata", (req, res) => {
+  console.log(req.body);
+  res.send("Data saved to db successfully!");
+});
+
+app.delete("/delete", (req, res) => {
+  res.send("Data deleted successfully!");
+});
+
+app.put("/put", (req, res) => {
+  res.send("Data Updated successfully!");
+});
+
+app.patch("/patch", (req, res) => {
+  res.send("Partial Data updated successfully!");
+});
 app.listen(port, () => {
   console.log(`Server is successfully listening on ${port}...`);
 });
